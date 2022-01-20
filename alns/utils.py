284080cb155cfa00cc377b80f68ff362dfb62a32
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2022-01-20 15:55:41 -0300

    Added function to save generated instances into an excel file

--- alns/utils.py
+++ alns/utils.py
@@ -3,6 +3,7 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 BEST = 0
 BETTER = 1
 ACCEPTED = 2
@@ -17,7 +18,8 @@ def is_acceptable(state):
     return 0
 
 
-def plot_graph(G: nx.Graph, output='plotgraph.png', terminals=True, solution=None) -> None:
+def plot_graph(G: nx.Graph, output='plotgraph.png', 
+        terminals=True, solution=None) -> None:
     """
     Plots the given graph with its costs
     """
@@ -26,14 +28,16 @@ def plot_graph(G: nx.Graph, output='plotgraph.png', terminals=True, solution=Non
         for g in G.edges(data=True)}
 
     node_labels = {
-        node:data['prize'] for node, data in G.nodes(data=True)
+        node:data['prize'] for node, data 
+            in G.nodes(data=True)
     }
 
     pos = nx.spring_layout(G)
     nx.draw_networkx(G, pos=pos, labels=node_labels)
     nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=labels)
     if solution is not None:
-        nx.draw_networkx_edges(G, pos, edgelist=solution.edges(), edge_color='r', width=2)
+        nx.draw_networkx_edges(G, pos, 
+            edgelist=solution.edges(), edge_color='r', width=2)
     if terminals:
         terminals_n = [n for n, data in G.nodes(data=True) if data['terminal']]
         nx.draw_networkx_nodes(G, pos, nodelist=terminals_n, node_color='green')
@@ -79,18 +83,3 @@ def parse_file(file_name: str) -> nx.Graph:
 
     return G
 
-
-if __name__ == "__main__":
-    # test_file = "data/steinc1-wmax_100-seed_33000-gw.dat"
-    test_file = "data/test.edges"
-
-    G = nx.Graph()
-    G.add_edge('A', 'B', cost=4)
-    G.add_edge('B', 'D', cost=2)
-    G.add_edge('A', 'C', cost=3)
-    G.add_edge('C', 'D', cost=4)
-    plot_graph(G)
-
-    G = parse_file(test_file)
-    plot_graph(G)
-# %%
--- generate_instances.py
+++ generate_instances.py
@@ -1,11 +1,19 @@
-import alns.instance_generator.instance_generator as ig
-import matplotlib.pyplot as plt
+# %%
+import os
 import pickle
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import alns.instance_generator.instance_generator as ig
+
 from alns.utils import plot_graph
 from alns.operators import repair_operators as ro
 
+from typing import List, Tuple
 
-def generate_multiple_instances(parameters):
+
+def generate_multiple_instances(parameters:
+        Tuple[int, int, int]) -> None:
     n = len(parameters)
     for i in range(1, n+1):
         num_nodes, num_terminals, num_edges = parameters[i-1]
@@ -20,6 +28,76 @@ def generate_multiple_instances(parameters):
         solution = ro.greedy_initial_solution(graph)
 
         plot_graph(graph, output=f"data/toys/toy_generated-{i}.png", solution=solution)
+
+
+def instances_to_excel() -> None:
+    """
+    Saves the toy instances into an excel file
+    """
+    path = "data/toys/"
+    file_names = [f"{path}{f}" 
+        for f in os.listdir(path) if f.endswith(".pickle")]
+    
+    graphs = []
+    for file_name in file_names:
+        with open(file_name, "rb") as p:
+            graphs.append(pickle.load(p))
+
+    graph_to_excel(graphs)
+
+
+def graph_to_excel(Gs: List[nx.Graph]) -> None:
+    """
+    Converts a list of graphs into (urgh) excel
+    """
+    def node_to_df(G: nx.Graph) -> \
+            Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Converts nodes info to a pandas dataframe
+        """
+        # this is ugly af
+        node_info = { k:
+            {"terminal":int(v["terminal"]), "prize":v["prize"], 
+                "x":v["pos"][0], "y":v["pos"][1]}
+            for k, v in G.nodes(data=True)}
+        
+        edges_info = { i:
+            {"u": data[0], "v":data[1], 
+                "cost": data[2]["cost"]}
+            for i, data in enumerate(G.edges(data=True))}
+        
+        return (pd.DataFrame(node_info).T,
+            pd.DataFrame( edges_info).T)
+
+    dfs = list(map(node_to_df, Gs))
+
+    def write_excel(dfs: List[pd.DataFrame], 
+            filename: str, sheet_names: List[str]):
+        """
+        Writes the dataframe to excel
+        """
+        writer = pd.ExcelWriter(filename,
+            engine='xlsxwriter')   
+        
+        for i, (mdfs, sheet_name) in enumerate(
+                zip(dfs, sheet_names)):
+            row = 0
+            for df in mdfs:
+                df.to_excel(writer, sheet_name=sheet_name,
+                    startrow=row, startcol=0)   
+                row = row + len(df.index) + 5
+            
+            worksheet = writer.sheets[sheet_name]
+            img_name = f"data/toys/toy_generated-{i+1}.png"
+            worksheet.insert_image("H4", img_name)
+        
+        writer.save()
+
+    sheet_names = [f"BT{i+1}" for i in range(len(dfs))]
+    write_excel(dfs, "instances_alns.xlsx", sheet_names)
+
+    return dfs
+
 
 if __name__ == "__main__":
     parameters = (
@@ -38,4 +116,7 @@ if __name__ == "__main__":
         (25, 11, 50),
     )
     generate_multiple_instances(parameters)
+    instances_to_excel()
 
+
+# %%
