d5c69e8e481b1694ee4499785a5de02b29ebcdd5
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2022-01-28 10:44:24 -0300

    Added preprocessing for terminals

--- alns/improvement.py
+++ alns/improvement.py
@@ -2,14 +2,13 @@
 import copy
 import random
 import networkx as nx
-from itertools import product
 
 from typing import Any, Dict, List, Tuple
 
 from alns.solution_instance import SolutionInstance
 
-Node = Tuple[int, Dict[Any, Any]]
-Edge = Tuple[int, int, int]
+Node = Tuple[int, Dict[str, Any]]
+Edge = Tuple[int, int, Dict[str, int]]
 
 degree_of_destruction = 0.25
 evaluate = SolutionInstance.evaluate
@@ -17,26 +16,64 @@ evaluate = SolutionInstance.evaluate
 
 ### preprocess ###
 
-def remove_leaves(G: nx.Graph) -> Tuple[Tuple[Node], 
-        Tuple[Edge], nx.Graph]:
+def remove_leaves(G: nx.Graph) -> nx.Graph:
     """
     Remove graph leaves and returns the resulting graph
     as well as the nodes taken out
     """
     nG = copy.deepcopy(G)
     
-    def _preprocess(nG: nx.Graph) -> Tuple[Node, Edge]:
-        try:
-            nodes, edges = zip(*[(node, edge) 
-                for node, edge in zip(nG.nodes(data=True), 
-                                      G.edges(data=True))
-                    if nG.degree(node[0])==1])
-        except ValueError:
-            return ((), ())
+    def _preprocess(nG: nx.Graph) -> None:
+        nodes = [node
+            for node in nG.nodes(data=True)
+            if nG.degree(node[0])==1 and not node[1]["prize"]]
+        if not nodes:
+            return
     
         nG.remove_nodes_from([n[0] for n in nodes])
+        _preprocess(nG)
+
+    _preprocess(nG)
+    return nG
+
+
+def terminal_leaves(G: nx.Graph) -> Tuple[
+    List[Node], List[Edge], nx.Graph]:
+    """
+    Merges terminal leaves into nodes. Assumes that
+    the only leaves are terminals
+    """
+    nG = copy.deepcopy(G)
+    
+    def _preprocess(nG: nx.Graph)-> Tuple[
+            List[Node], List[Edge], nx.Graph]:
+        nodes = [node
+            for node in nG.nodes(data=True)
+            if nG.degree(node[0])==1]
+        if not nodes:
+            return ([], [])
+
+        keep_nodes = []
+        for node in nodes:
+            # it only has one edge
+            edge = list(nG.edges([node[0]], data=True))[0]
+            linked_node = nG.nodes[edge[1]]
+
+            # calculate reward for getting terminal
+            add_prize = node[1]["prize"] - edge[-1]["cost"]
+            # if the reward is positive we keep it
+            if add_prize > 0:
+                linked_node["prize"] += add_prize
+                linked_node["terminal"] = True
+                keep_nodes.append(node)
+
+        edges = G.edges([n[0] for n in keep_nodes],
+            data=True)
+        nG.remove_nodes_from([n[0] for n in nodes])
+
+        # return the nodes and edges to reconstruct graph
         nnodes, nedges = _preprocess(nG)
-        return (nodes + nnodes, edges + nedges)
+        return ((keep_nodes + nnodes), list(edges) + nedges)
 
     return _preprocess(nG) + (nG,)
 
