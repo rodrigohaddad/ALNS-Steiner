9032de0a32d8cfb34cfc116cae61a3fd054e9bec
henriquejsfj <henriquepilbil1@gmail.com>
2022-01-30 21:44:19 -0300

    Use multithreading to run different instances

--- alns_steiner.py
+++ alns_steiner.py
@@ -1,12 +1,15 @@
 import os
+from math import log
+import pickle
+from tkinter.tix import MAX
+from matplotlib import pyplot as plt
+from time import sleep, time
+import threading
+
 from alns import statistics, utils
 import alns.improvement as imp
 from alns.simmulated_annealing import SimulatedAnnealing
 from alns.solution_instance import SolutionInstance
-from math import log
-import pickle
-from matplotlib import pyplot as plt
-from time import time
 
 
 ''' ALNS for Steiner prize collecting problem
@@ -17,6 +20,7 @@ problem optimization.'''
 
 FILEPATH = 'data/toys'
 RESULTPATH = 'data/results'
+MAX_THREADS = 16
 
 
 def t_function_1(t: float, t0: float, beta=0.9) -> float:
@@ -47,43 +51,66 @@ def _get_instances():
             yield utils.parse_file(file), filename
 
 
+def _process(G, filename, **params):
+    results_list = []
+    statistics_list = []
+    timing_list = []
+
+    for _ in range(5):
+        t0 = time()
+        initial_solution = SolutionInstance(G, imp.greedy_initial_solution(G))
+
+        sa = SimulatedAnnealing(initial_solution=initial_solution, **params)
+        result = sa.simulate()
+        
+        elapsed = time() - t0
+
+        results_list.append(result)
+        statistics_list.append(statistics_list)
+        timing_list.append(elapsed)
+
+    result_dict = {
+        "results": results_list,
+        "statistics": statistics_list,
+        "timing": timing_list
+    }
+
+    result_filename = os.path.join(RESULTPATH, f'results-{filename}.pickle')
+    with open(result_filename, 'wb') as result_file:
+        pickle.dump(result_dict, result_file)
+
+
+def _wait_threads(threads, limit=MAX_THREADS):
+    while len(threads) >= limit:
+        for i, t in enumerate(threads):
+            if not t.is_alive():
+                threads.pop(i)
+        
+        sleep(2)
+
 def main():
     params = {'temperature': 250,
             't_function': t_function_2,
             'alns_scores': [7, 3.5, 1, 0],
             'alns_decay': 0.8,
             'alns_n_iterations': 500}
+
+    threads = []
     for G, filename in _get_instances():
 
         G = imp.remove_leaves(G)
 
-        results_list = []
-        statistics_list = []
-        timing_list = []
+        if len(threads) >= MAX_THREADS:
+            _wait_threads(threads)
 
-        for _ in range(5):
-            t0 = time()
-            initial_solution = SolutionInstance(G, imp.greedy_initial_solution(G))
+        threads.append(
+            threading.Thread(target=_process, args=(G, filename), kwargs=params)
+        )
 
-            sa = SimulatedAnnealing(initial_solution=initial_solution, **params)
-            result = sa.simulate()
-            
-            elapsed = time() - t0
+        threads[-1].start()
 
-            results_list.append(result)
-            statistics_list.append(statistics_list)
-            timing_list.append(elapsed)
-
-        result_dict = {
-            "results": results_list,
-            "statistics": statistics_list,
-            "timing": timing_list
-        }
-
-        result_filename = os.path.join(RESULTPATH, f'results-{filename}.pickle')
-        with open(result_filename, 'wb') as result_file:
-            pickle.dump(result_dict, result_file)
-
+    _wait_threads(threads, limit=1)
+        
 
 if __name__ == "__main__":
     main()
